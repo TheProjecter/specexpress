4a5b3a98c82c9f7410b917e9f065ebf658f4f2cb
technoag <technoag@abb21b06-7deb-11de-9693-657164a66812>
2009-08-06 20:46:45 +0000

    Refactored Length Tests to isolate code tested to just the RuleValidator.

---
+++ SpecExpress/src/SpecExpress/Rules/StringValidators/Numeric.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace SpecExpress.Rules.StringValidators
+//{   
+//    public class Numeric<T> : RuleValidator<T, string>
+//    {
+//        public override object[] Parameters
+//        {
+//            get { return new object[] {}; }
+//        }
+
+//        public override ValidationResult Validate(RuleValidatorContext<T, string> context)
+//        {
+//            context.PropertyValue.Contains()
+//            return Evaluate(length <= _max, contextWithLength);
+//        }
+//    }
+//}

84c428984f894a20324dfcf778144eb000dc69de
technoag <technoag@abb21b06-7deb-11de-9693-657164a66812>
2009-08-28 02:49:40 +0000

    Updated SpecificationScanner to use Linq

--- trunk/SpecExpress/src/SpecExpress/SpecificationScanner.cs
+++ trunk/SpecExpress/src/SpecExpress/SpecificationScanner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 
 namespace SpecExpress
@@ -28,36 +29,31 @@ namespace SpecExpress
 
         public void AddAssembly(Assembly assembly)
         {
-            registerValidTypeInAssembly(assembly);
+            registerAssemblies(new List<Assembly>() { assembly });            
         }
 
 
         public void AddAssembliesFromPath(string path)
         {
-            IEnumerable<string> assemblyPaths = Directory.GetFiles(path).Where(file =>
+            var r = Directory.GetFiles(path).Where(file =>
                                                                                Path.GetExtension(file).Equals(
                                                                                    ".exe",
                                                                                    StringComparison.OrdinalIgnoreCase)
                                                                                ||
                                                                                Path.GetExtension(file).Equals(
                                                                                    ".dll",
-                                                                                   StringComparison.OrdinalIgnoreCase));
+                                                                                   StringComparison.OrdinalIgnoreCase))
+                                                                                   .Select(assemblyPath => Assembly.LoadFrom(assemblyPath));
 
-            foreach (string assemblyPath in assemblyPaths)
-            {
-                Assembly assembly = null;
-                try
-                {
-                    assembly = Assembly.LoadFrom(assemblyPath);
-                }
-                catch
-                {
-                }
-                if (assembly != null && assembly != typeof(ValidationCatalog).Assembly)
-                {
-                    AddAssembly(assembly);
-                }
-            }
+           
+            List<Assembly> assemblies = r.Where<Assembly>(assembly => assembly != null && assembly != typeof(ValidationCatalog).Assembly)
+                .ToList<Assembly>();
+
+            registerAssemblies(assemblies);
+
+
+                 
+
         }
 
         private Assembly findTheCallingAssembly()
@@ -79,28 +75,21 @@ namespace SpecExpress
             return callingAssembly;
         }
 
-        private void registerValidTypeInAssembly(Assembly assembly)
+        private void registerAssemblies(List<Assembly> assemblies)
         {
-            foreach (Type type in assembly.GetExportedTypes())
-            {
-                if (typeof (Specification).IsAssignableFrom(type))
+            //Find all types in all assemblies that inherit from Specification
+            IEnumerable<Type> specs = from a in assemblies
+                        select a.GetExportedTypes() into types
+                        from type in types
+                        where typeof(Specification).IsAssignableFrom(type)
+                        select type;
+
+            //For each type, instantiate it and add it to the collection of specs found
+            specs.ToList<Type>().ForEach(spec => 
                 {
-                    object o = Activator.CreateInstance(type);
-
-                    Type keyType = o.GetType().BaseType.GetGenericArguments().FirstOrDefault();
-
-                    try
-                    {
-                        //Register
-                        _specifications.Add(o as Specification);
-                    }
-                    catch (ArgumentException argumentException)
-                    {
-                        throw new ArgumentException(
-                            "A Specification for class " + keyType + " has been loaded already.", argumentException);
-                    }
-                }
-            }
+                    object o = Activator.CreateInstance(spec);
+                    _specifications.Add(o as Specification);
+                });
         }
     }
 }

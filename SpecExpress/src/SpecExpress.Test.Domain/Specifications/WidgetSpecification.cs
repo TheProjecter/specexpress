49fb822801f36216bb69f47459d3774f3448a069
technoag <technoag@abb21b06-7deb-11de-9693-657164a66812>
2009-08-30 04:25:15 +0000

    Implemented ValidationCatalog.AssertConfigurationIsValid()

---
+++ SpecExpress/src/SpecExpress.Test.Domain/Specifications/WidgetSpecification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SpecExpress.Test.Domain.Entities;
+
+namespace SpecExpress.Test.Domain.Specifications
+{
+    public class WidgetSpecification : SpecificationBase<Widget>
+    {
+        public WidgetSpecification()
+        {
+            Check(w => w.Name);
+        }
+    }
+}
---
+++ SpecExpress/src/SpecExpress/SpecExpressConfigurationError.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpecExpress
+{
+    [Serializable]
+    public class SpecExpressConfigurationError : ApplicationException
+    {
+        public SpecExpressConfigurationError(string message)
+            : base(message)
+        {
+
+        }
+
+    }
+}

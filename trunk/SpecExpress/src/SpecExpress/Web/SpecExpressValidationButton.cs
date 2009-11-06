6eaed2cda32833bd109d181b065faee746518cb8
technoag@gmail.com <technoag@gmail.com@abb21b06-7deb-11de-9693-657164a66812>
2009-11-06 04:12:38 +0000

    Added ASP.Net Button to ease of use with using SpecExpress on ASP.Net apps.

---
+++ trunk/SpecExpress/src/SpecExpress/Web/SpecExpressValidationButton.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI.WebControls;
+
+namespace SpecExpress.Web
+{
+    public class ValidationNotificationEventArgs
+    {
+        private ValidationNotification _ve;
+        public ValidationNotificationEventArgs(ValidationNotification vn)
+        {
+            _ve = vn;
+        }
+
+        public ValidationNotification ValidationNotification
+        {
+            get { return _ve;}
+        }
+    }
+
+    public class ValidationButton : System.Web.UI.WebControls.Button
+    {
+        //EventHandlers
+        public delegate object GetObjectHandler();
+        public delegate void ValidationNotificationHandler(object sender, ValidationNotificationEventArgs e);
+        //Events
+        public event ValidationNotificationHandler ValidationNotification;
+        public event GetObjectHandler GetObject;
+
+
+        protected void OnValidationNotification(ValidationNotificationEventArgs e)
+        {
+            if (ValidationNotification != null)
+            {
+                ValidationNotification(this, e);
+            }
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            //if (GetObject == null)
+            //{
+            //    //Can't validate entire object
+            //    //so validate by property
+            //}
+
+            if (GetObject != null)
+            {
+                //Get the object to validate
+                var validatingObject = GetObject();
+
+                //Get the Specification from the Page
+                var spec = ValidationCatalog.GetAllSpecifications().Where(
+                   x => x.GetType() == ((IPageSpecification)Page).PageSpecification).FirstOrDefault();
+
+
+                //Validate the object using the ValidationCatalog
+                var vldNotification = ValidationCatalog.Validate(validatingObject, spec);
+                
+                //Invalid
+                //Raise notificaiton to controls
+                if (!vldNotification.IsValid)
+                {
+                    var helper = new SpecExpressProxyValidatorHelper(Page);
+                    helper.Notify(vldNotification);
+
+                    //Raise OnValidationNotification Event
+                    OnValidationNotification(new ValidationNotificationEventArgs(vldNotification));
+                }
+                else
+                {
+                    //If valid, then raise OnClick Event
+                    base.OnClick(e);
+                }
+            }
+        }
+    }
+}

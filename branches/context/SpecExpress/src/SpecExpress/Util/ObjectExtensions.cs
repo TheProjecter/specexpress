b65dec4edf8e01d70efe22231b9799e207ec881a
technoag@gmail.com <technoag@gmail.com@abb21b06-7deb-11de-9693-657164a66812>
2010-02-15 04:07:52 +0000

    Properly handle Required rules on Bool types when the value was False.

--- branches/context/SpecExpress/src/SpecExpress/Util/ObjectExtensions.cs
+++ branches/context/SpecExpress/src/SpecExpress/Util/ObjectExtensions.cs
@@ -9,9 +9,20 @@ namespace SpecExpress.Util
     public static class ObjectExtensions
     {
         public static bool IsNullOrDefault<TProperty>(this TProperty input)
-        {
-            return (input == null
-                         || input.Equals(string.Empty)
+        {   
+            if (input == null)
+            {
+                return true;
+            }
+
+            //Ignore default values for booleans
+            if (input.GetType() == typeof(bool))
+            {
+                return false;
+            }
+
+            //Evaluate special cases
+            return ( input.Equals(string.Empty)
                          || Equals(input, default(TProperty))
                          || !(  !(input is IEnumerable) ||
                                 (input is IEnumerable && ((IEnumerable) (input)).GetEnumerator().MoveNext()))
